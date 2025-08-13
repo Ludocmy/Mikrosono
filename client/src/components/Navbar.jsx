f1e2b52b8b92659ce04f18df2d26b109142d8496
Ludovic Commanay <ludocommanay@icloud.com>
2025-08-13 17:34:48 +0100

    Navbar responsiveness

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -6,59 +6,69 @@ import SVGIcon from "../assets/MikrosonoLogo.svg?react";
 
 export default function Navbar() {
   const [searchTerm, setSearchTerm] = React.useState(''); 
+  const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false)
 
   return (
-    <div className ="sticky top-0 left-0 bg-grey p-4 pr-5 pl-5 z-50">
-      <nav className=" flex items-center">
+    <nav className ="sticky top-0 left-0 bg-grey p-4 pr-5 pl-5 z-50">
+      <div className="flex items-center justify-between">
+        <div className="flex items-center">
 
             <NavLink to="/">
                 <SVGIcon alt="Mikrosono logo" className="h-18 w-18 hover:fill-blue-600"/>
             </NavLink>
 
-            <div className = "flex-auto w-10"></div>
+          </div>
+
+          <div className="hidden sm:flex items-center space-x-4">
 
             <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
 
-            <NavLink className="flex items-end justify-end whitespace-nowrap pr-5 pl-5 not-first:font-gravity text-3xl font-stretch-extended ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none bg-background hover:text-blue-600" to="/create">
+            <NavLink className="font-gravity text-black text-3xl hover:text-blue-600" to="/create">
               Radio Shows
             </NavLink>
 
-            <NavLink className="hidden items-end justify-end whitespace-nowrap pr-5 pl-5 font-gravity text-3xl font-stretch-extended ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none bg-background hover:text-blue-600 sm:flex" to="/create">
+            <NavLink className="font-gravity text-black text-3xl hover:text-blue-600" to="/create">
               Events
             </NavLink>
 
-            {/* {isMobileMenuOpen && (
-                <div className="mt-4 sm:hidden">
-                    <input
-                        type="text"
-                        placeholder="Search"
-                        className="p-2 rounded-md bg-gray-700 text-white w-full mb-2"
-                    />
-                    <button className="bg-blue-500 text-white py-2 px-4 rounded-md w-full mb-2">
-                        Search
-                    </button>
-                    <div className="space-y-2">
-                        <Link to="/cart" className="text-white flex items-center">
-                            <FiShoppingCart className="mr-1" />
-                            Cart
-                            <span className='bg-blue-500 text-white rounded-full px-2 py-1 ml-2'>{cartItems.length}</span>
-                        </Link>
+          </div>
 
-                        {userInfo && <div className="relative group ">
-                            {renderProfileButton()}
-                        </div>}
+          <div className="sm:hidden">
+              <button onClick={() => setIsMobileMenuOpen(!isMobileMenuOpen)} className="text-black font-gravity text-4xl focus:outline-none">
+                  ☰
+              </button>
+          </div>
 
-                        {userInfo?.isAdmin && <div className="relative group ">
-                            {renderAdminButton()}
-                        </div>}
+      </div>
 
-                        {!userInfo && renderSignInButton()}
+      {isMobileMenuOpen && (
+          <div className="flex-col items-center justify-center mt-8 sm:hidden">
 
-                    </div>
-                </div>
-            )} */}
+              <div className="w-full justify-center text-center p-4">
 
-      </nav>
-    </div>
+                <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
+              
+              </div>
+
+              <div className="w-full justify-center text-center p-3">
+
+                <NavLink className="font-gravity text-black text-3xl hover:text-blue-600" to="/create">
+                  Radio Shows
+                </NavLink>
+
+              </div>
+
+              <div className="w-full justify-center text-center p-3">
+
+                <NavLink className="font-gravity text-black text-3xl hover:text-blue-600" to="/create">
+                  Events
+                </NavLink>
+
+              </div>
+              
+          </div>
+      )}
+
+    </nav>
   );
 }
--- client/src/components/Search.jsx
+++ client/src/components/Search.jsx
@@ -3,7 +3,7 @@ import SearchIcon from "../assets/search.svg?react";
 
 const Search = ({ searchTerm, setSearchTerm }) => {
   return (
-    <div className="w-40  p-1 rounded-lg mx-5 border-black border-1">
+    <div className="p-1 rounded-lg mx-5 border-black border-1">
       <div className='relative flex items-center'>
         <SearchIcon alt="Search Icon" className="absolute left-2 h-3 w-3"/>
 
